7d4eb249c399fa2eeba0b55e3b752f67956e11ac
will <gunmonyk@gmail.com>
2025-03-28 11:56:02 +0100

    completing delete todo button, and adding css for todo list view

--- demo/testapp/urls.py
+++ demo/testapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.home, name="Base"),
     path("todos/", views.todos, name="todos"),
     path("home_page/", views.home, name="Home page"),
-    path("add_todo_item/",  views.add_todo_item, name="add_todo_item")
+    path("add_todo_item/",  views.add_todo_item, name="add_todo_item"),
+    path('delete/<int:todo_id>/', views.delete_todo_item, name='delete_todo_item'),
 ]
--- demo/testapp/views.py
+++ demo/testapp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, HttpResponse, redirect
+from datetime import datetime
+
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from django.utils.timezone import make_aware
+
 from .models import TodoList
 
 def base(request):
@@ -15,6 +19,19 @@ def add_todo_item(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         completed = request.POST.get('completed') == "on"
-        TodoList.objects.create(title=title, completed=completed)
+        deadline = request.POST.get('deadline')
+        if deadline:
+            naive_deadline = datetime.strptime(deadline, "%Y-%m-%dT%H:%M")
+            aware_deadline = make_aware(naive_deadline)
+        else:
+            aware_deadline = None
+        TodoList.objects.create(title=title, completed=completed, deadline_day=aware_deadline)
         return redirect('todos')
+
     return render(request, "add_todo_item.html")
+
+def delete_todo_item(request, todo_id):
+    if request.method == "POST":
+        todo = get_object_or_404(TodoList, id=todo_id)
+        todo.delete()
+        return redirect("todos")
