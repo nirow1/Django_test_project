2c6187b48f71fdc71d1c7a3b8ff89235a7e63410
nirow <gunmonyk@gmail.com>
2025-05-13 14:01:21 +0200

    bug fix list names not saving, first changes on shopping list view

--- demo/testapp/views.py
+++ demo/testapp/views.py
@@ -11,28 +11,6 @@ def base(request):
 def home(request):
     return render(request, "home_page.html")
 
-def add_shopping_list(request):
-    print("res")
-    if request.method == "POST":
-        # Get the shopping list name from the request
-        print("post")
-        shopping_list_name = request.POST.get('shopping_list_name',
-                                              'Unnamed List')  # Default to 'Unnamed List' if empty
-        shopping_list = ShoppingList.objects.create(name=shopping_list_name)  # Create the ShoppingList instance
-
-        # Get all the items from the form
-        item_names = request.POST.getlist('name')  # Retrieve all "name" inputs from the form
-
-        # Create Items for the ShoppingList
-        for item_name in item_names:
-            if item_name.strip():  # Check that the item name is not empty
-                Item.objects.create(name=item_name.strip(), shopping_list=shopping_list)
-
-        return redirect('/home_page')  # Redirect to a page that lists all shopping lists or success page
-
-        # If GET, render the form to create a shopping list
-    return render(request, 'add_shopping_list.html')
-
 def todos(request):
     items = TodoList.objects.all()
     return render(request, "todos.html", {"todos": items})
@@ -75,8 +53,26 @@ def change_state(request, todo_id):
         todo.save()
         return redirect(todos)
 
+def add_shopping_list(request):
+    print("res")
+    if request.method == "POST":
+        shopping_list_name = request.POST.get('list-name',
+                                              'Unnamed List')
+        shopping_list = ShoppingList.objects.create(name=shopping_list_name)
+        
+        # Get all the items from the form
+        item_names = request.POST.getlist('name')  # Retrieve all "name" inputs from the form
+
+        for item_name in item_names:
+            if item_name.strip():  
+                Item.objects.create(name=item_name.strip(), shopping_list=shopping_list)
+
+        return redirect('/home_page')
+
+    return render(request, 'add_shopping_list.html')
+
 def shopping_lists(request):
     shopping_lists = ShoppingList.objects.prefetch_related('items').all()
     for list in shopping_lists:
-        print(list)
+        pass
     return render(request, 'shopping_lists.html', {'shopping_lists': shopping_lists})
